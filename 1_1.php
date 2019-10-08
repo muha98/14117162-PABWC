eaef93168861f8eab76ca2f659368d9d709e9e78
muha98 <muhasfk123@gmail.com>
2019-10-09 00:30:35 +0700

    test

--- 1_1.php
+++ 1_1.php
@@ -1,13 +1,1 @@
-<html>
-<head>
-    <title> Contoh Skrip PHP </title>
-</head>
-<body>
-    <?php
-    $nama = "Kim Taehyung";
-    echo "$nama berkata \"save me save me\" ";
-    ?>
-</body>
-</html>
-
-
+test
